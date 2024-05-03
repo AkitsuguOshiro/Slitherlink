dc9298dd291adc70c4977bdba02deb870e6e1149
Akitsugu Oshiro <aktg222@outlook.jp>
2024-05-03 21:16:19 +0900

    modified:   slither2.c

--- slither2.c
+++ slither2.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
 int main ()
 {
-    struct Status
-    {
-    int number;//盤面に書いてある数字0,1,2,3のどれか、空白だと-1として記述。
-    int group;//内外の状態が同じものを同じgroupにする。盤面の外側(確定)のgroupは-1、内側(確定)は1、未確定の場合は正負のみ気にする。最終的に二つのグループに分かれることを目指す。
-    }status[100][100];//status[縦][横]
+
 
 }
